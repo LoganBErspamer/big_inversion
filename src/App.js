c74830091fe2879931c0cc9efd7f0d7ca67ccc71
Logan <loganerspamer@gmail.com>
2022-05-05 13:07:44 -0700

    Functional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PersonCard firstName="Logan" 
+      lastName="Erspamer" 
+      age={20} 
+      hairColor="Black"/>
+
+      <PersonCard firstName="Sam" 
+      lastName="Sotsky" 
+      age={21} 
+      hairColor="Blonde"/>
+
+      <PersonCard firstName="Kyle" 
+      lastName="Deato" 
+      age={25} 
+      hairColor="Black"/>
+
+      <PersonCard firstName="Lexington" 
+      lastName="Carey" 
+      age={19} 
+      hairColor="Red"/>
     </div>
   );
 }
